58ad1ab23bdab3e59db11e24ae2f217b9a6fbab5
Rafael Kayo <rafael.kayo99@gmail.com>
2021-11-15 17:31:40 -0500

    using useCase to list categories

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -7,6 +7,7 @@ interface IRequest {
 
 class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
+
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
